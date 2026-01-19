1ea9f6e300ca9832eea1b60960dc476a6f55f2a8
Xen0nkidd <nicholas.donovan0802@gmail.com>
2026-01-18 22:36:05 -0600

    Update counter.js

--- counter.js
+++ counter.js
@@ -3,6 +3,14 @@ fetch("https://api.countapi.xyz/hit/Xen0nkidd/XNCount")
   .then(d => {
     document.getElementById("num").textContent = d.value;
   })
+  setInterval(() => {
+  fetch("https://api.countapi.xyz/get/Xen0nkidd/XNCount")
+    .then(r => r.json())
+    .then(d => {
+      document.getElementById("num").textContent = d.value;
+    });
+}, 5000);
+
   .catch(() => {
     document.getElementById("num").textContent = "blocked";
   });
